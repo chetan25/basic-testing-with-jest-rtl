2975ba30ff8c40fe1e395902726de4e485652977
Chetan Dasauni <chetan.dasauni@unblunce.com>
2022-02-17 08:10:57 -0800

    added more sample code

--- samples/first-attempt-hook.js
+++ samples/first-attempt-hook.js
@@ -1,10 +1,8 @@
 import { useState, useEffect } from "react";
-
 import axios from "axios";
-
 import style from "./Card.module.css";
 
-const Card = () => {
+const useUserData = () => {
     const [userData, setUserData] = useState(null);
 
     useEffect(() => {
@@ -23,19 +21,29 @@ const Card = () => {
         fetchData();
     }, []);
 
+    return {
+        gender: userData?.gender || "N/A",
+        email: userData?.email || "N/A",
+        location: userData?.location || "N/A",
+        title: userData?.title || "N/A",
+        image: userData?.image || "",
+    };
+};
+
+const Card = ({ title, location, email, gender, image }) => {
+    const { title, location, email, gender, image } = useUserData();
+
     return (
         <section className={style.card}>
-            <img className={style.cardImage} src={userData.image} alt={userData.title} />
+            <img className={style.cardImage} src={image} alt={title} />
             <div className={style.cardContent}>
-                <h3 className={style.cardTitle}>{userData.title}</h3>
+                <h3 className={style.cardTitle}>{title}</h3>
                 <span className={style.cardLocation}>{location}</span>
                 <div className={style.cardContact}>
-                    <span className={style.cardMail}>{`email: ${userData.email}`}</span>
-                    <span className={style.cardGender}>{`gender: ${userData.gender}`}</span>
+                    <span className={style.cardMail}>{`email: ${email}`}</span>
+                    <span className={style.cardGender}>{`gender: ${gender}`}</span>
                 </div>
             </div>
         </section>
     );
 };
-
-export default Card;
